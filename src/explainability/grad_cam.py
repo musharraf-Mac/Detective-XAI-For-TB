ec23bc85e1b37ba81e3920294df1ee59e8545924
Zaffraj-Suzanee <suzaneemusadique59@gmail.com>
2026-08-03 21:02:35 +0530

    feat: implement Grad-CAM heatmap generation

--- src/explainability/grad_cam.py
+++ src/explainability/grad_cam.py
@@ -1,32 +1,82 @@
 """
-Grad-CAM implementation skeleton.
-
-Used to generate visual explanations
-for TB classification models.
+Grad-CAM implementation for TB X-ray model explanation.
 """
+
+import torch
+import torch.nn.functional as F
 
 
 class GradCAM:
 
     def __init__(self, model, target_layer):
-        """
-        Initialize Grad-CAM.
-
-        Args:
-            model: trained deep learning model
-            target_layer: layer used for visualization
-        """
-
+        
         self.model = model
         self.target_layer = target_layer
 
+        self.gradients = None
+        self.activations = None
 
-    def generate(self, image):
-        """
-        Generate heatmap.
+        self._register_hooks()
 
-        This will be implemented
-        in the next commits.
-        """
 
-        pass
+    def _register_hooks(self):
+
+        def forward_hook(module, input, output):
+            self.activations = output.detach()
+
+
+        def backward_hook(module, grad_input, grad_output):
+            self.gradients = grad_output[0].detach()
+
+
+        self.target_layer.register_forward_hook(
+            forward_hook
+        )
+
+        self.target_layer.register_full_backward_hook(
+            backward_hook
+        )
+
+
+    def generate(self, image, class_index=None):
+
+        self.model.zero_grad()
+
+        output = self.model(image)
+
+
+        if class_index is None:
+            class_index = torch.argmax(output)
+
+
+        loss = output[:, class_index]
+
+        loss.backward()
+
+
+        gradients = self.gradients
+        activations = self.activations
+
+
+        weights = torch.mean(
+            gradients,
+            dim=(2,3),
+            keepdim=True
+        )
+
+
+        heatmap = torch.sum(
+            weights * activations,
+            dim=1
+        )
+
+
+        heatmap = F.relu(heatmap)
+
+
+        heatmap = heatmap.squeeze()
+
+        heatmap /= torch.max(heatmap)
+
+
+        return heatmap.cpu().numpy()
